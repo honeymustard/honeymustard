fc23a6f428305176cceed079cd2abf1700992f0f
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2017-11-07 17:31:49 +0100

    adds more syles and state to head

--- src/components/Head/index.jsx
+++ src/components/Head/index.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
 
+import 'components/Head/head.scss';
+
 class Head extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            open: false
+        };
+    }
+
+    buttonClick(e) {
+        this.setState({open: !this.state.open});
+    }
+
+    toggleClass(state) {
+        return state.open ? 'menu-open' : '';
+    }
 
     render() {
         return (
             <header className="head">
-                #Head
+              <div className="area">
+                <nav className={'menu ' + this.toggleClass(this.state)}>
+                  <div id="logo">
+                    <img src="/" />
+                  </div>
+
+                  <button onClick={this.buttonClick.bind(this)} className="menu-trigger"></button>
+
+                  <div className="menu-triggee">
+                      <ul className="menu-list">
+                          <li><a href="/">link</a></li>
+                      </ul>
+                  </div>
+                </nav>
+              </div>
             </header>
         );
     }
