f7128603e43f5ed3c9f3749dd9aaa4ef4eef3f8c
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2017-11-08 20:32:52 +0100

    adds initial api route

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var path = require('path');
+var mongoose = require('mongoose');
 var app = express();
 
+/* Serve all files from dist folder */
 app.use('/dist', express.static(path.join(__dirname, '/dist')));
 
+/* Serve data from api */
+app.get('/api/horse', function (req, res) {
+    res.send('Horseey..');
+});
+
+/* The frontend will handle other requests */
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
