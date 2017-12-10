ec78583ea32c59dca3bf64a18e1ab26a93d87226
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2017-12-10 13:52:08 +0100

    adds elementary output for jobs archive

--- src/components/Archives/jobArchive.jsx
+++ src/components/Archives/jobArchive.jsx
@@ -6,10 +6,44 @@ import { Layout } from 'components/Layouts';
  */
 class JobArchive extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: [],
+        };
+    }
+
+    componentDidMount() {
+        fetch('/api/jobs', {
+            Accept: 'application/json',
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            this.setState({items: data});
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+
     render() {
+        let items = this.state.items;
         return (
             <Layout>
-                <p>jobs jobs jobs</p>
+                {items ? (
+                    <ul className="list">
+                        {items.map(item => {
+                            return (
+                                <li key={item._id}>
+                                    <h3>{item.company}</h3>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                ) : (
+                    <p>No jobs at this time</p>
+                )}
             </Layout>
         );
     }
