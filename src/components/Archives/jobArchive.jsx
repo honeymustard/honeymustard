2df5749f06aa7a989f3bc76eec2018fdd6d2e848
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2017-12-10 11:08:25 +0100

    adds jobs archive page

---
+++ src/components/Archives/jobArchive.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Layout } from 'components/Layouts';
+
+/**
+ * Jobs listing page.
+ */
+class JobArchive extends React.Component {
+
+    render() {
+        return (
+            <Layout>
+                <p>jobs jobs jobs</p>
+            </Layout>
+        );
+    }
+}
+
+export default JobArchive;
