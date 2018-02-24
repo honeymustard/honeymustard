a9b5608a767c143fe4ea8fa496baa7dcbd7fe06d
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2018-02-24 16:56:45 +0100

    adds dates to jobs listing

--- src/components/Items/JobItem.jsx
+++ src/components/Items/JobItem.jsx
@@ -3,13 +3,30 @@ import TechList from 'components/Lists/TechList';
 
 import './job-item.scss';
 
+let pad = n => n < 10 ? `0${n}` : n;
+let formatDate = isoDate => {
+  let d = new Date(isoDate);
+
+  return [
+    pad(d.getDate()),
+    pad(d.getMonth()+1),
+    d.getFullYear(),
+  ].join('.');
+}
+
 const JobItem = ({item}) => (
   <li className="job-item" key={item._id}>
-    <h3 className="job-item-title">
+    <h3 className="job-title">
       <a href={item.url}>
         {item.company}
       </a>
     </h3>
+
+    <span className="job-date">
+      {formatDate(item.startDate)} -
+      {item.endDate ? formatDate(item.endDate) : 'Present'}
+    </span>
+
     <p>{item.tagline}</p>
 
     <TechList list={item.technologies} />
