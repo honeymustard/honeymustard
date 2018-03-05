72847b6d2dbad04932c44561d5c721abbd2f4347
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2018-03-05 21:20:25 +0100

    makes minor changes to footer

--- src/components/Foot/index.jsx
+++ src/components/Foot/index.jsx
@@ -4,33 +4,35 @@ import { Link } from 'react-router-dom';
 import './foot.scss';
 
 const Foot = (props) => (
-  <footer className="foot grid-row">
-    <div className="foot-col">
-      <section>
-        <h3>Info</h3>
-        <p>
-          Copyright © 2018<br />
-          <Link to="/">Honeymustard.org</Link>
-        </p>
-      </section>
-    </div>
+  <footer className="foot">
+    <div className="foot-row grid-row">
+      <div className="foot-col">
+        <section>
+          <h3>Info</h3>
+          <p>
+            Copyright © 2018<br />
+            <Link to="/">Honeymustard.org</Link>
+          </p>
+        </section>
+      </div>
 
-    <div className="foot-col">
-      <section>
-        <h3>Contact</h3>
-        <p>
-          Send me some post<br />
-          <a href="mailto:post@honeymustard.org">post@honeymustard.org</a>
-        </p>
-      </section>
-    </div>
+      <div className="foot-col">
+        <section>
+          <h3>Contact</h3>
+          <p>
+            Send me some post<br />
+            <a href="mailto:post@honeymustard.org">post@honeymustard.org</a>
+          </p>
+        </section>
+      </div>
 
-    <div className="foot-col">
-      <section>
-        <h3>Sourcecode</h3>
-        <p>You can find all of my public projects on <a href="https://github.com/honeymustard">github.com!</a>
-        </p>
-      </section>
+      <div className="foot-col">
+        <section>
+          <h3>Sourcecode</h3>
+          <p>You can find all of my public projects on <a href="https://github.com/honeymustard">github.com!</a>
+          </p>
+        </section>
+      </div>
     </div>
   </footer>
 );
