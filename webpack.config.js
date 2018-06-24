bb49a147aa56841f5f661ca01a34a458be0938da
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2018-06-24 12:55:01 +0200

    adds prod settings to style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,59 @@
 const path = require('path');
+const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    entry: './src/index.jsx',
-    output: {
-        publicPath: '/dist/',
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'mustard.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.(js|jsx)$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.(css|scss)$/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader'
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            includePaths: ['src']
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(png|svg)$/,
-                loader: 'file-loader'
+  entry: './src/index.jsx',
+  output: {
+    publicPath: '/dist/',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'mustard.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: {
+              hmr: !prod,
+              singleton: prod
             }
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: ['src']
+            }
+          }
         ]
-    },
-    resolve: {
-        modules: [
-            path.resolve('./src'),
-            path.resolve('./node_modules')
-        ],
-        extensions: ['.js', '.jsx']
-    },
-    devServer: {
-        progress: false,
-        inline: true,
-        proxy: {
-            "**": "http://localhost:3000"
-        }
+      },
+      {
+        test: /\.(png|svg)$/,
+        loader: 'file-loader'
+      }
+    ]
+  },
+  resolve: {
+    modules: [
+      path.resolve('./src'),
+      path.resolve('./node_modules')
+    ],
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    progress: false,
+    inline: true,
+    proxy: {
+        '**': 'http://localhost:3000'
     }
+  }
 }
